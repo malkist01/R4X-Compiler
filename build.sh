f00efc2f59d6f48ed4fb8426da207a0874b14f30
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-08-01 13:20:01 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -90,7 +90,7 @@ add_kernelsu() {
   cd build/kernel
 
   # integrate kernelsu-next
-  curl -sSL "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s v1.0.6
+  curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -
   # prepare .config
   make "${MAKE_FLAGS[@]}" $KERNEL_CONFIG
 
